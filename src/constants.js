e043b240766e5503ad38623ba6f038e3559b868c
jackmchandler-tech <jack.m.chandler@gmail.com>
2026-03-19 11:33:33 -0400

    Create constants.js
    Breaking apart App.jsx to increase safety of the build

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const STORAGE_KEY = "bread-gifting-tracker-v1";
+export const APP_VERSION = "v1.2.4";
+
+export const DEFAULT_APP_SETTINGS = {
+  title: "Bread Tracker",
+  itemSingular: "loaf",
+  itemPlural: "loaves",
+  enableRatings: true,
+  enableFeedback: true,
+};
