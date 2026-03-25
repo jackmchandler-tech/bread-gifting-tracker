3d660f84937b5facd12900f5c608b066e42a7a37
jackmchandler-tech <jack.m.chandler@gmail.com>
2026-03-25 18:31:19 -0400

    Update AddPersonToGroupModal.jsx
    added default to the export function definition

--- src/components/AddPersonToGroupModal.jsx
+++ src/components/AddPersonToGroupModal.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-export function AddPersonToGroupModal({ person, availableGroups, onClose, onAdd }) {
+export default function AddPersonToGroupModal({ person, availableGroups, onClose, onAdd }) {
   return (
     <Modal title={`Add ${person?.name || "Person"} to Group`} onClose={onClose}>
       <div className="space-y-2">
