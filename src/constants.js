c0333ca737d699125cee612c5471941e4bcdd0d4
jackmchandler-tech <jack.m.chandler@gmail.com>
2026-03-28 13:05:59 -0400

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const STORAGE_KEY = "bread-gifting-tracker-v1";
-export const APP_VERSION = "v1.3.4;
+export const APP_VERSION = "v1.3.4";
 
 export const DEFAULT_APP_SETTINGS = {
   title: "Bread Tracker",
